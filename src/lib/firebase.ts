7c3e12f8b126ad6ca9b3a99d3793964ec0a1d2a7
Troy Carter <twc561@gmail.com>
2025-07-09 15:28:07 +0000

    can you incorporate the following code and APIs to fix my firebase authe

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyAKOZL2TdqleyUtbrbKHJfjObRqpBz2s28",
+  authDomain: "florida-shield.firebaseapp.com",
+  projectId: "florida-shield",
+  storageBucket: "florida-shield.firebasestorage.app",
+  messagingSenderId: "320998993928",
+  appId: "1:320998993928:web:79043e80e835f9c3eee1bd"
 };
+
 
 let app: FirebaseApp | null = null;
 let auth: Auth | null = null;
