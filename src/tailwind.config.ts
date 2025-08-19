dd2403c9dc3a637f56a23f794c25a9cec1d0c796
Troy Carter <twc561@gmail.com>
2025-08-19 21:33:05 +0000

    any other errors that would prevent a  build errors

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -74,7 +74,7 @@ export default {
       },
       borderRadius: {
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
+        md: 'calc(var(--radius) - 4px)',
         sm: 'calc(var(--radius) - 8px)',
       },
       keyframes: {
